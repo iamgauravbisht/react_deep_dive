468616cc5eed4d001fd41d69eb7837eb3cc6e900
Gaurav <gauravbisht154@gmail.com>
2024-01-07 21:01:42 +0530

    useMemo practice

--- src/App.jsx
+++ src/App.jsx
@@ -1,70 +1,54 @@
-// import {} from "react";
+import { useEffect } from "react";
+import { useMemo } from "react";
+import { useState } from "react";
 
 // function App() {
+//   const [n, setN] = useState(0);
+//   const [count, setCount] = useState(0);
+//   function countValue() {
+//     setCount(0);
+//     for (let i = 0; i < n; i++) {
+//       setCount((prev) => prev + i);
+//     }
+//   }
+
 //   return (
 //     <div>
-//       <CardWrapper>
-//         <CardWrapper>
-//           <h1>Gaurav bisht</h1>
-//         </CardWrapper>
-//         <h2>GodLike</h2>
-//       </CardWrapper>
+//       <input onChange={(e) => setN(e.target.value)} />
+//       <br />
+//       <button onClick={countValue}>Click to Calculate</button>
+//       <br />
+//       Sum from 1 to n is: {count};
 //     </div>
 //   );
 // }
 
-// function CardWrapper({ children }) {
-//   return (
-//     <div style={{ border: "1px solid black", padding: "10px" }}>{children}</div>
-//   );
-// }
-
-// export default App;
-
-import { useEffect, useState } from "react";
 function App() {
-  const [todos, setTodos] = useState([]);
+  const [n, setN] = useState(0);
   const [count, setCount] = useState(0);
 
   useEffect(() => {
-    // alert("hello");
-    setInterval(() => {
-      fetch("https://sum-server.100xdevs.com/todos").then(async (res) => {
-        const json = await res.json();
-        setTodos(json.todos);
-      });
-    }, 10000);
-  }, []);
+    console.log("inside useEffect");
+    let count = 0;
+    for (let i = 0; i <= n; i++) {
+      count += i;
+    }
+    setCount(count);
+  }, [n]);
 
-  // console.log(todos);
-  function updateCounter() {
-    setCount((prev) => prev + 1);
-  }
-  return (
-    <CardWrapper>
-      <button
-        onClick={updateCounter}
-        style={{ border: "2px solid green", padding: "15px 30px" }}
-      >
-        counter : {count}
-      </button>
-      <p>no. of todos : {todos.length}</p>
-      {todos.length === 0 && <h1>Loading...</h1>}
-      {todos.map((todo) => {
-        return (
-          <div key={todo.id}>
-            <h1>{todo.title}</h1>
-            <p>{todo.description}</p>
-          </div>
-        );
-      })}
-    </CardWrapper>
-  );
-}
+  let countValue = useMemo(() => {
+    console.log("inside useMemo");
+    return count + 5;
+  }, [count]);
 
-function CardWrapper({ children }) {
   return (
-    <div style={{ border: "1px solid black", padding: "10px" }}>{children}</div>
+    <div>
+      <input onChange={(e) => setN(e.target.value)} />
+      <br />
+      <button>Click to Calculate</button>
+      <br />
+      Sum from 1 to n + 5 is: {countValue}
+    </div>
   );
 }
 
